fd674822db6a172483ff698401dd705542929baa
nschirmer02 <nschirmer02@gmail.com>
2025-03-18 10:15:18 -0400

    Updating required packages

--- code/01_LoadPackages.R
+++ code/01_LoadPackages.R
@@ -1,5 +1,9 @@
 #Package contains the read_tsv function. Ecotaxa files are in .tsv format and require this function to be read into R
 library(readr)
+
+#Streamlining file paths for calling in data
+library(here)
+
 #Packages utilized for efficient and readable dataframe mutations
 library(dplyr)
 library(tidyverse)
